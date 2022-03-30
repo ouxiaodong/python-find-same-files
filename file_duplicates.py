dc0acba0adf488a08d5efd60f56b2293758f3bca
ouxiaodong <mihoyi@163.com>
2022-03-30 11:21:08 +0800

    Update file_duplicates.py

--- file_duplicates.py
+++ file_duplicates.py
@@ -14,7 +14,7 @@ def printUsage():
 *********************************
 
 脚本功能：找出指定路径下是否存在内容完全一致的文件
-使用方式：python tgaduplicates.py 检索路径
+使用方式：python duplicates.py 检索路径
 产出结果：当前工作路径下duplicates.txt
 
 *********************************
